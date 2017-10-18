ec58c6ef402b4bca3f8e1e390605756d7d79de0e
zarrvis <zarrvis@gmail.com>
2017-10-18 10:47:36 +0900

    vue js test

---
+++ resources/views/phonebook.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  </head>
+  <body>
+    <div id="app">
+
+    </div>
+    <script src="{{ asset('js/app.js') }}" charset="utf-8"></script>
+  </body>
+</html>
